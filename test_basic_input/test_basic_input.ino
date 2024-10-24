9b14b8607c4c433393c6633ddd8f1f294693cca1
Lonny <lonnymckiernan@gmail.com>
2024-10-24 09:30:37 +0100

    Change basic_input to test line following

--- test_basic_input/test_basic_input.ino
+++ test_basic_input/test_basic_input.ino
@@ -2,16 +2,17 @@
 IDP
 can be used for any sensor just to test if you have the connections right
 */
-int inputPin = 10;  // select the input pin
+int inputPin = 2;  // select the input pin
 void setup() {
   //Serial init
   Serial.begin(9600);
+  pinMode(inputPin, INPUT);
 }
 
 void loop() {
   // read the value from the sensor:
-  int input_t = analogRead(inputPin) * 100;
+  int input_t = digitalRead(inputPin);
 
-  Serial.print(input_t);
+  Serial.println(input_t);
   delay(500);
 }
